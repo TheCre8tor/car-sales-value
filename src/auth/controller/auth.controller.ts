6b7fa4dbab23475a13929474562b1957455b6411
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-01-13 04:23:39 +0100

    getting the current user

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,22 +1,26 @@
-import { Body, Controller, Post, Session } from '@nestjs/common';
+import { Body, Controller, Get, Post, Session } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import CreateUserDto from 'src/users/dtos/create-user.dto';
 import SigninDto from '../dto/signin.dto';
 import { User } from 'src/users/entity/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import UserDto from 'src/users/dtos/user.dto';
+import { UsersService } from 'src/users/service/users.service';
 
 @Serialize(UserDto)
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly service: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
 
   @Post('/signup')
   async createUser(
     @Body() body: CreateUserDto,
     @Session() session: any,
   ): Promise<User> {
-    const user = await this.service.signup(body);
+    const user = await this.authService.signup(body);
     session.userId = user.id;
     return user;
   }
@@ -26,8 +30,13 @@ export class AuthController {
     @Body() body: SigninDto,
     @Session() session: any,
   ): Promise<User> {
-    const user = await this.service.signin(body.email, body.password);
+    const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
     return user;
+  }
+
+  @Get('/whoami')
+  async whoAmI(@Session() session: any) {
+    return await this.userService.findOne(session.userId);
   }
 }
