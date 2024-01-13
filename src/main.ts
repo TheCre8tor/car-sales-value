3edd5dc339ebd0e9523dc5bbf77e4b53817a4861
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-01-13 03:53:36 +0100

    set up a session

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // cookie session middleware ->
+  app.use(cookieSession({ keys: ['x-security-1867-000'] }));
+
   await app.listen(3000);
 }
 bootstrap();
